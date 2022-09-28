012b0637683ba6f6fa7ac6943ad5917abe498af0
Mario <mario.ohashi@gmail.com>
2022-09-28 19:02:58 -0300

    Create header.php

---
+++ trabalho_php/header.php
@@ -0,0 +1,11 @@
+<div class="header">
+    <div class="logo">
+        <img src="img/php-logo.svg" alt="Logo PHP" width="50">
+    </div>
+    <div class="navbar">
+        <a href="?p=home">Home</a>
+        <a href="?p=form1">Formulario 1</a>
+        <a href="?p=form2">Formulario 2</a>
+        <a href="?p=form3">Formulario 3</a>
+    </div>
+</div>
